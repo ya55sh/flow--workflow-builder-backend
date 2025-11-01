3b3cf9992dd07d64d841d9a7c58931adcc9f4949
ya55sh <yashbahuguna10@gmail.com>
2025-11-01 15:45:03 +0530

    replaced resend with nodemailer

--- src/mailService/mail.service.ts
+++ src/mailService/mail.service.ts
@@ -1,40 +1,19 @@
-import nodemailer from 'nodemailer';
 import { Injectable } from '@nestjs/common';
-import { Transporter } from 'nodemailer';
 
+import { Resend } from 'resend';
+const resend = new Resend(process.env.RESEND_API_KEY);
 // Create a test account or replace with real credentials.
 @Injectable()
 export class MailService {
-  private transporter: Transporter;
-
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: 'gmail',
-      secure: false,
-      auth: {
-        user: `${process.env.GMAIL_USER}@gmail.com`,
-        pass: `${process.env.GMAIL_APP_PASSWORD}`,
-      },
-    });
-    try {
-      this.transporter.verify();
-      console.log('Email connection verified');
-    } catch (error) {
-      console.error('Error verifying email connection:', error);
-      throw new Error('Failed to verify email connection');
-    }
-  }
   async sendEmail(to: string, subject: string, text: string) {
-    console.log('email sent');
     try {
-      const mailOptions = {
-        from: `${process.env.GMAIL_USER}@gmail.com`,
-        to,
-        subject,
-        text,
-      };
-      await this.transporter.sendMail(mailOptions);
-      console.log('Email sent successfully');
+      await resend.emails.send({
+        from: `${process.env.SEND_EMAIL_USER}`,
+        to: `${to}`,
+        subject: `${subject}`,
+        html: `${text}`,
+      });
+      console.log('Email sent successfully', to);
     } catch (error) {
       console.error('Error sending email:', error);
       throw new Error('Failed to send email');
